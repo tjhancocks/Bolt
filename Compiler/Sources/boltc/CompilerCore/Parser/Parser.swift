e2593ebebc2e669df1ad662f12c47c9a051a1fd5
Tom Hancocks <tomhancocks@gmail.com>
2019-04-17 12:54:13 +0100

    Remove all old parser code
    Just rip out the old parser and rewrite. Refactoring from one
    architecture to another was not really going to work in any reasonable
    or sane way, so lets just go for it...
    
    Broken builds for a little while now.

--- Compiler/Sources/boltc/CompilerCore/Parser/Parser.swift
+++ Compiler/Sources/boltc/CompilerCore/Parser/Parser.swift
@@ -22,45 +22,10 @@ class Parser {
     private(set) var tokenStream: TokenStream
     private(set) var scanner: Scanner<[Token]>
 
-    lazy var parsers: [ParserHelperProtocol] = {
-        return Parser.rootParsers
-    }()
-
     init(tokenStream: TokenStream) {
         self.tokenStream = tokenStream
         self.scanner = tokenStream.scanner
     }
-}
-
-// MARK: - Parser Lookup
-
-extension Parser {
-
-    static var rootParsers: [ParserHelperProtocol] {
-        return [
-            FunctionParser(),
-            ImportParser(),
-            VariableParser()
-        ]
-    }
-
-    static var scopedParsers: [ParserHelperProtocol] {
-        return returnParsers + [
-            ReturnParser(),
-            VariableParser()
-        ]
-    }
-
-    static var returnParsers: [ParserHelperProtocol] {
-        return [
-            StringLiteralParser(),
-            FloatLiteralParser(),
-            IntegerLiteralParser(),
-            CallParser(),
-            IdentifierParser()
-        ]
-    }
-
 }
 
 // MARK: - Parsing
@@ -69,42 +34,10 @@ extension Parser {
 
     @discardableResult
     func parse() throws -> AbstractSyntaxTree {
-        let ast = AbstractSyntaxTree(mainModuleName: tokenStream.file.moduleName)
-
-        while scanner.available {
-            ast.add(try parseNextExpression(from: scanner, ast: ast))
-        }
-        
-        return ast
-    }
-
-    func parseNextExpression(from scanner: Scanner<[Token]>, ast: AbstractSyntaxTree) throws -> AbstractSyntaxTree.Node {
-        // Loop through the parsers and find one that matches.
-        for parser in parsers {
-            if parser.test(for: scanner) {
-                return try parser.parse(from: scanner, ast: ast)
-            }
-        }
-
-        // If we get here, then we must assume that we could not recognise
-        // the sequence of tokens
-        if let token = scanner.peek() {
-            throw Error.parserError(location: scanner.location, reason: .unrecognised(token: token))
-        }
-        else {
-            throw Error.parserError(location: scanner.location, reason: .unexpectedEndOfTokenStream)
-        }
+        throw Error.parserError(location: .unknown, reason: .unexpectedEndOfTokenStream)
     }
 
 }
-
-// MARK: - Parser Protocol
-
-protocol ParserHelperProtocol {
-    func test(for scanner: Scanner<[Token]>) -> Bool
-    func parse(from scanner: Scanner<[Token]>, ast: AbstractSyntaxTree) throws -> AbstractSyntaxTree.Node
-}
-
 
 // MARK: - Scanner Extensions
 
