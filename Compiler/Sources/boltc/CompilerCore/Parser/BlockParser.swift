07f13ffe998d413baf2b9254d564eccc582f963d
Tom Hancocks <tomhancocks@gmail.com>
2019-04-18 07:14:28 +0100

    Add an initial BlockParser for function bodies.
    The primary purpose of the block parser is to parse expressions in the
    form of `'{' [ <expr> ...] '}'`. This is typically found after function
    declarations, in order to produce function definitions.

---
+++ Compiler/Sources/boltc/CompilerCore/Parser/BlockParser.swift
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 Tom Hancocks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+class BlockParser: SubParserProtocol {
+
+    static func test(scanner: Scanner<[Token]>) -> Bool {
+        return scanner.test(expected:
+            .symbol(symbol: .leftBrace, mark: scanner.location)
+        )
+    }
+
+    static func parse(scanner: Scanner<[Token]>, owner parser: Parser) throws -> AbstractSyntaxTree.Expression {
+        let location = scanner.location
+        var expressions: [AbstractSyntaxTree.Expression] = []
+
+        // Assume that the '{' is present. The test should have been run first, or we've
+        // been invoked from an area that expects it.
+        scanner.advance()
+
+        blockParser: while scanner.available {
+            // If we hit a '}' then break out of the group.
+            if scanner.test(expected: .symbol(symbol: .rightBrace, mark: scanner.location)) {
+                break blockParser
+            }
+
+            expressions.append(try parser.parseNextExpression())
+        }
+
+        // Return a block expression to the caller.
+        return .block(expressions, location: location)
+    }
+
+}
